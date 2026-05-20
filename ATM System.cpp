5deeb1f0545f3647e5117f3a8c13641fb0bf9487
Abdulrahman.Mohammad <hussinatabood46@gmail.com>
2026-05-20 15:14:16 +0300

    Update ATM System.cpp

--- ATM System.cpp
+++ ATM System.cpp
@@ -36,9 +36,9 @@ vector<string> SplitString(string S1, string Delim)
     vector<string> vString;
 
     short pos = 0;
-    string sWord; // define a string variable  
+    string sWord; 
 
-    // use find() function to get the position of the delimiters  
+    
     while ((pos = S1.find(Delim)) != std::string::npos)
     {
         sWord = S1.substr(0, pos); // store the word   
@@ -47,12 +47,12 @@ vector<string> SplitString(string S1, string Delim)
             vString.push_back(sWord);
         }
 
-        S1.erase(0, pos + Delim.length());  /* erase() until positon and move to next word. */
+        S1.erase(0, pos + Delim.length());
     }
 
     if (S1 != "")
     {
-        vString.push_back(S1); // it adds last word of the string.
+        vString.push_back(S1); 
     }
 
     return vString;
@@ -71,8 +71,7 @@ sClient ConvertLinetoRecord(string Line, string Seperator = "#//#")
     Client.PinCode = vClientData[1];
     Client.Name = vClientData[2];
     Client.Phone = vClientData[3];
-    Client.AccountBalance = stod(vClientData[4]);//cast string to double
-
+    Client.AccountBalance = stod(vClientData[4]);
 
     return Client;
 
@@ -99,7 +98,7 @@ vector <sClient> LoadCleintsDataFromFile(string FileName)
     vector <sClient> vClients;
 
     fstream MyFile;
-    MyFile.open(FileName, ios::in);//read Mode
+    MyFile.open(FileName, ios::in);
 
     if (MyFile.is_open())
     {
@@ -146,7 +145,7 @@ vector <sClient> SaveCleintsDataToFile(string FileName, vector <sClient> vClient
 {
 
     fstream MyFile;
-    MyFile.open(FileName, ios::out);//overwrite
+    MyFile.open(FileName, ios::out);
 
     string DataLine;
 
@@ -158,7 +157,7 @@ vector <sClient> SaveCleintsDataToFile(string FileName, vector <sClient> vClient
 
             if (C.MarkForDelete == false)
             {
-                //we only write records that are not marked for delete.  
+                 
                 DataLine = ConvertRecordToLine(C);
                 MyFile << DataLine << endl;
 
@@ -243,7 +242,7 @@ short getQuickWithDrawAmount(short QuickWithDrawOption)
 
 void PerfromQuickWithdrawOption(short QuickWithDrawOption)
 {
-    if (QuickWithDrawOption == 9)//exit 
+    if (QuickWithDrawOption == 9) 
         return;
 
     short WithDrawBalance = getQuickWithDrawAmount(QuickWithDrawOption);
